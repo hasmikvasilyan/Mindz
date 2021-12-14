4244bc4f9dcd28d40a05d5904fab66edae7c4d4e
hasmikvasilyan <h.vasilyan@gmail.com>
2021-12-14 17:57:07 +0400

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import {Fragment} from "react";
+import Header from "./Header/Header";
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <Fragment>
+
+    
+    <Header></Header>
+    <img src={logo}/>
+    <div></div>
+    </Fragment>
+  );
+}
+
+export default App;
